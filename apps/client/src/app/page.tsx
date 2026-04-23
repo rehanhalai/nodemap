e4eaa247001a8bd089f21822b1284716bc9a3d0e
Rehan Halai <halairehan01@gmail.com>
2026-04-23 23:34:57 +0530

    feat: implement socket.io greeting functionality in client app

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { io } from "socket.io-client";
+import { useState } from "react";
+
 export default function HomePage() {
+  const emtiHelloMSG = () => {
+    const socket = io("http://localhost:8080");
+    socket.emit("greeting", "fuck you");
+  };
+
   return (
     <main>
       <h1>Client App</h1>
-      <p>Next.js app is running inside Turborepo.</p>
+      <button onClick={() => emtiHelloMSG()}>Emit Greeting Message</button>
     </main>
   );
 }
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,10 +4,18 @@ import { Server } from "socket.io";
 
 const app = exrpess();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {});
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("user connected: ", socket.id);
+
+  socket.on("greeting", (msg) => {
+    io.emit("greeting", `Hello from server! Received message: ${msg}`);
+  });
 });
 
 httpServer.listen(8080);
